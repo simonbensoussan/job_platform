3f4b937976c80a465a877abd38cda56b19796c5b
Slim Antoine <antoine.vaucamps@gmail.com>
2016-08-12 18:51:49 +0200

    Test template, index method in views and index url

--- signUp/models.py
+++ signUp/models.py
@@ -4,10 +4,11 @@ from django.db import models
 
 # Create your models here.
 
+
 class SignUp(models.Model):
     username = models.CharField(max_length=254)
     email = models.EmailField()
-    date = models.DateTimeField(verbose_name="Date inscription",auto_now=False,auto_now_add=True)
+    date = models.DateTimeField(verbose_name="Date inscription", auto_now=False, auto_now_add=True)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 
     def __str__(self):
